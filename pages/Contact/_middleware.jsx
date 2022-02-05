480bc5c411f8a975187a728b0633b6e26cef6c9f
foryoubluee <dakarawebdev@gmail.com>
2022-02-05 21:51:21 +0700

    update contact page
    update contact pages

--- pages/Contact/_middleware.jsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import React from 'react';
-import UnderConstruct from '../../components/UnderConstruct';
-
-const _middleware = () => {
-  return NextResponse.UnderConstruct
-};
-
-export default _middleware;
--- pages/Contact/index.jsx
+++ pages/Contact/index.jsx
@@ -1,5 +1,8 @@
 import Layout from '../../components/Layout'
 import Head from 'next/head'
+import Image from 'next/image'
+import logoWA from '../../public/whatsapp-logo.png'
+import logoIG from '../../public/instagram.png'
 
 const Contact = () => {
    return (
@@ -7,8 +10,13 @@ const Contact = () => {
          <Head>
             <title>Contact us</title>
          </Head>
-         <div className='grid place-content-center h-screen w-full'>
-            <a href="https://api.whatsapp.com/send?phone=085607200065">Whatsapp</a>
+         <div className='flex justify-center items-center h-screen w-full'>
+            <a className='h-[20vh] w-[20vh]' href="https://wa.me/+6281398467317">
+               <Image src={logoWA} alt='whatsapp' /> 
+            </a>
+            <a className='h-[13vh] w-[13vh]' href="https://www.instagram.com/rasl_catalog/">
+               <Image src={logoIG} alt='instagram' /> 
+            </a>
          </div>
       </Layout>
    )
